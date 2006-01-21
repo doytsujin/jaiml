f2acf29a9fa187ce9aac47b562451e333a988ea8
Kim Sullivan <k@sulli.cz>
2006-01-21 00:40:47 +0000

    load() method aiml element handling

--- src/aiml/parser/AIMLParser.java
+++ src/aiml/parser/AIMLParser.java
@@ -7,23 +7,71 @@ package aiml.parser;
  * @version 1.0
  */
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+
+import org.xmlpull.v1.XmlPullParserException;
 
 import junit.framework.*;
 import junit.textui.TestRunner;
 
 
 public class AIMLParser {
-
+  
   public AIMLParser() {
     
+  }
+  
+  private void doAiml(XmlParser parser) throws IOException, XmlPullParserException, AimlParserException {
+    parser.nextTag();
+    try {
+      parser.require(XmlParser.START_TAG,null,"aiml");
+      String version = parser.getAttributeValue(null,"version");
+      if (version==null)
+        throw new AimlSyntaxException("Syntax error, version attribute of root element must be present");
+      if (!version.equals("1.0") && !version.equals("1.0.1"))
+        throw new InvalidAimlVersionException("Unsupported AIML version, refusing forward compatible processing mode");
+      
+    } catch(XmlPullParserException e) {
+      throw new AimlSyntaxException("Syntax error, root element must be 'aiml'",e);
+    }
+    //doCategoryList(parser);
+  }
+  
+  public void load(Reader in) throws IOException, XmlPullParserException, AimlParserException {
+    XmlParser parser = new XmlParser();
+    parser.setInput(in);
+    doAiml(parser);
   }
   
   public class AIMLParserTest extends TestCase {
     public AIMLParserTest(String s){
       super(s);
     }
+    public void testAimlRoot() throws Exception {
+      load(new StringReader("<aiml version='1.0'/>"));
+      
+      try {
+        load(new StringReader("<aiml></aiml>"));
+        fail("Expected AimlSyntaxException");
+      } catch (AimlSyntaxException e) {};
+
+      try {
+        load(new StringReader("<AIML></AIML>"));
+        fail("Expected AimlSyntaxException");
+      } catch (AimlSyntaxException e) {};
+      
+      try {
+        load(new StringReader("<aiml version='1.0p'></aiml>"));
+        fail("Expected InvalidAimlVersionException");
+      } catch (InvalidAimlVersionException e) {};
+      
+      
+    }
+
   }
   
   private AIMLParserTest getTest(String name) {
