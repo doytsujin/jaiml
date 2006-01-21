6079a447634d6f6e79eaf8cf0b146deadd181ced
Kim Sullivan <k@sulli.cz>
2006-01-21 00:40:15 +0000

    Added some basic exceptions

---
+++ src/aiml/parser/AimlParserException.java
@@ -0,0 +1,25 @@
+package aiml.parser;
+
+public class AimlParserException extends Exception {
+
+  public AimlParserException() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  public AimlParserException(String arg0) {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+  public AimlParserException(String arg0, Throwable arg1) {
+    super(arg0,arg1);
+    // TODO Auto-generated constructor stub
+  }
+
+  public AimlParserException(Throwable arg0) {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+}
---
+++ src/aiml/parser/InvalidAimlVersionException.java
@@ -0,0 +1,25 @@
+package aiml.parser;
+
+public class InvalidAimlVersionException extends AimlParserException {
+
+  public InvalidAimlVersionException() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  public InvalidAimlVersionException(String arg0) {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+  public InvalidAimlVersionException(String arg0, Throwable arg1) {
+    super(arg0,arg1);
+    // TODO Auto-generated constructor stub
+  }
+
+  public InvalidAimlVersionException(Throwable arg0) {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+}
