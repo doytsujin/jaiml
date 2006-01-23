910f5882a5b104835635fa02e734373064d14511
Kim Sullivan <k@sulli.cz>
2006-01-23 01:25:22 +0000

    Added static Bots class that keeps track of all the different bots

---
+++ src/aiml/bot/DuplicateBotException.java
@@ -0,0 +1,25 @@
+package aiml.bot;
+
+public class DuplicateBotException extends Exception {
+
+  public DuplicateBotException() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  public DuplicateBotException(String arg0) {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+  public DuplicateBotException(String arg0, Throwable arg1) {
+    super(arg0,arg1);
+    // TODO Auto-generated constructor stub
+  }
+
+  public DuplicateBotException(Throwable arg0) {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+}
