cdc9b1e28764a6ea5770c69c157357db74421fef
Kim Sullivan <k@sulli.cz>
2006-01-30 01:39:18 +0000

    Added command line processing

--- src/demo/InterpreterDemo.java
+++ src/demo/InterpreterDemo.java
@@ -1,9 +1,9 @@
 package demo;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,6 +20,18 @@ import aiml.parser.AimlParserException;
 public class InterpreterDemo {
   public static void main(String[] args) throws XmlPullParserException, IOException, BotSyntaxException, AimlParserException {  
     Logger.getLogger("aiml").setLevel(Level.WARNING);
+    System.out.println("Demonstration program for the java aiml interpreter library");
+    System.out.println("(C) Kim Sullivan, 2006");
+    if (args.length==0) {
+      System.err.println("Error: you must specify a command line argument with the bot to load!");
+      return;
+    } else {
+      File f = new File(args[0]);
+      if (!f.exists()) {
+        System.err.println("Error: file not found : " + f);
+        return;
+      }
+    }
     Classifier.registerDefaultNodeHandlers();
     Bot b = new Bot();
     System.out.println("Loading bot...");
@@ -31,16 +43,18 @@ public class InterpreterDemo {
     StringContext input = (StringContext)ContextInfo.getContext("input");
     
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("> ");
     String line = in.readLine();
     while (!line.equals("/exit")) {
-      line = in.readLine();
       input.setValue(line);      
       MatchState m = Classifier.match();
-      if (m!=null)
-        System.out.println(((aiml.script.Script)m.getResult()).execute(m));
-      else
+      if (m!=null) {
+        System.out.println(((aiml.script.Script)m.getResult()).execute(m, 0));        
+      } else
         System.out.println("no match found");
+      System.out.print("> ");
+      line = in.readLine();
     }
-    System.out.println("bye bye");
+    System.out.println("Terminanting.");
   }
 }
