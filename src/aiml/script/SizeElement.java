86ebf75d588f2b5c0cc08a225cf30954c9789708
Kim Sullivan <k@sulli.cz>
2008-01-02 21:34:33 +0000

    Implemented <size>

--- src/aiml/script/SizeElement.java
+++ src/aiml/script/SizeElement.java
@@ -1,11 +1,12 @@
 package aiml.script;
 
+import aiml.classifier.Classifier;
 import aiml.classifier.MatchState;
 
 public class SizeElement extends EmptyElement {
 
   public String evaluate(MatchState m) {
-    return "size()";
+    return String.valueOf(Classifier.getCount());
   }
 
   public String toString() {
