690d3d5870c6f4e2fae156a091eaac8ece95b1a2
Kim Sullivan <k@sulli.cz>
2008-02-29 11:29:28 +0000

    Updated copyright & added GPL boilerplate

--- src/aiml/substitutions/DuplicateSubstitutionException.java
+++ src/aiml/substitutions/DuplicateSubstitutionException.java
@@ -1,3 +1,17 @@
+/*
+    jaiml - java AIML library
+    Copyright (C) 2004-2008  Kim Sullivan
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package aiml.substitutions;
 
 /**
