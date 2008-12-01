36aef1145481909dfbfd894caf413d6f2812330f
Kim Sullivan <k@sulli.cz>
2008-12-01 23:42:50 +0000

    Added classes that support generation of graphviz graphs (for classifier graph visualization)

---
+++ src/graphviz/Graphviz.java
@@ -0,0 +1,161 @@
+/*
+    jaiml - java AIML library
+    Copyright (C) 2004-2008  Kim Sullivan
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package graphviz;
+
+/**
+ * This class provides utility methods for generating edges and nodes in the Graphviz language.
+ * @author Kim Sullivan
+ *
+ */
+public class Graphviz {
+  /**
+   * Indentation level.
+   */
+  private static int indent=0;
+  /**
+   * Generates indentation according to the current indentation level.
+   * @param sb
+   */
+  private static void indent(StringBuilder sb) {
+    for (int i=0;i<indent; i++) {
+      sb.append("\t");
+    }
+  }
+  
+  /**
+   * The epsilon symbol used in epsilon transitions
+   */
+  public static final String EPSILON = "epsilon";
+
+  /**
+   * A symbol that accepts all characters from the alphabet (used in wildcards)
+   */
+  public static final String ALPHABET = "T";
+
+  /**
+   * Generate a list of node or edge attributes according to the graphviz syntax 
+   * @param sb
+   * @param attributes an array of key - value strings.
+   * @return
+   */
+  private static StringBuilder attributes(StringBuilder sb, String... attributes) {
+    if (attributes.length >0) {
+      sb.append('[');
+      for (int i=0; i<attributes.length;i+=2) {
+        sb.append(attributes[i]);
+        sb.append("=\"");
+        sb.append(attributes[i+1]);
+        sb.append("\" ");
+      }
+      sb.append(']');
+    }
+
+    return sb;
+  }
+  
+  /**
+   * Generate a list of graph or subgraph attributes with the correct graphviz syntax.
+   * @param sb
+   * @param attributes an array of key - value strings.
+   * @return
+   */
+  public static StringBuilder graphAttributes(StringBuilder sb, String... attributes) {
+    if (attributes.length >0) {
+      indent(sb);
+      for (int i=0; i<attributes.length;i+=2) {
+        sb.append(attributes[i]);
+        sb.append("=\"");
+        sb.append(attributes[i+1]);
+        sb.append("\";\n");
+      }
+    }
+
+    return sb;
+  }
+  /**
+   * Generate a graphviz node (with optional attributes)
+   * @param sb
+   * @param id the node id
+   * @param attributes keys and values
+   * @return
+   */
+  public static StringBuilder node(StringBuilder sb,String id, String... attributes) {  
+    indent(sb);
+    sb.append(id);
+    attributes(sb,attributes);
+    sb.append(";\n"); 
+    return sb;
+  }
+  
+  /**
+   * Generate a graphviz edge between the two specified nodes.
+   * @param sb
+   * @param from source node id
+   * @param to destination node id
+   * @param attributes optional list of attributes
+   * @return
+   */
+  public static StringBuilder edge(StringBuilder sb, String from, String to, String...attributes) {
+    indent(sb);
+    sb.append(from);
+    sb.append("->");
+    sb.append(to);
+    attributes(sb,attributes);
+    sb.append(";\n");     
+    return sb;    
+  }
+  
+  /**
+   * Starts a new graph or subgraph, and increases indentation.
+   * @param sb
+   * @param what
+   * @return
+   */
+  public static StringBuilder start(StringBuilder sb, String what) {
+    indent(sb);
+    sb.append(what);
+    sb.append(" {\n");
+    indent++;
+    return sb;
+  }
+  /**
+   * Ends a graph or subgraph, and decreases indentation.
+   * @param sb
+   * @return
+   */
+  public static StringBuilder end(StringBuilder sb) {
+    indent--;
+    indent(sb);
+    sb.append("}\n");
+    return sb;
+  }
+  
+  /**
+   * Generates a labeled edge between a GraphvizNode and another,
+   * and generates the graph induced by the "to" node.
+   * @param sb
+   * @param from source node
+   * @param to destination graph
+   * @param edgeLabel edge label
+   * @return
+   */
+  public static StringBuilder connectGraph(StringBuilder sb, GraphvizNode from, GraphvizNode to, String edgeLabel) {
+    if (to!=null) {      
+      edge(sb, from.gvNodeID(), to.gvNodeID(),"label",edgeLabel);
+      to.gvGraph(sb);      
+    }
+    return sb;
+  }
+  
+}
---
+++ src/graphviz/GraphvizNode.java
@@ -0,0 +1,74 @@
+/*
+    jaiml - java AIML library
+    Copyright (C) 2004-2008  Kim Sullivan
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package graphviz;
+
+/** 
+ * <p>An interface that simplifies the generation of graphviz based graphs. It is used in
+ * the visualisation of the classification tree. It should not be used for graphs
+ * containing cycles.</p>
+ * <p>The methods that generate the textual description of the graph use a StringBuffer -
+ * each method accepts a StringBuffer as a destination for the textual representation, and it
+ * returns the modified buffer.</p>
+ * <p>There are two methods that generate edges from the node to the rest of the world. The first,
+ * <code>internalGraph()</code> generates edges and nodes that logically belong to the same graph
+ * (the same context, and use the same algorithm). The <code>externalGraph()</code> generates
+ * an "external" graph, where the context (and possibly the matching algorithm) is different.</code>
+ * 
+ * @author Kim Sullivan
+ *
+ */
+public interface GraphvizNode {
+
+  /**
+   * Returns the unique node ID of this graphviz node.
+   * @return
+   */
+  public abstract String gvNodeID();
+
+  /**
+   * Returns the display label of this node.
+   * @return
+   */
+  public abstract String gvNodeLabel();
+
+  /**
+   * Appends a textual description of the subgraph that has this node as root (possibly
+   * recursively calling the gvGraph() method of child nodes).
+   * @param sb the destination buffer
+   * @return the modified buffer
+   */
+  public abstract StringBuilder gvGraph(StringBuilder sb);
+
+  /**
+   * Appends a textual description of this node (without any edges).
+   * @param sb the destination buffer
+   * @return the modified buffer
+   */
+  public abstract StringBuilder gvNodes(StringBuilder sb);
+
+  /**
+   * Generates edges that belong to the same graph.
+   * @param sb the destination buffer
+   * @return the modified buffer
+   */
+  public abstract StringBuilder gvInternalGraph(StringBuilder sb);
+
+  /**
+   * Generates edges that belong to a different (sub)graph.
+   * @param sb the destination buffer
+   * @return the modified buffer
+   */
+  public abstract StringBuilder gvExternalGraph(StringBuilder sb);
+
+}
